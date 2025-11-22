d1acbdcc1562707784247f7265772299bcd851cf
Miracle-Aduma <miracle.aduma@pau.edu.ng>
2025-11-22 16:18:22 +0100

    9

--- week-6/practice_9/src/main.rs
+++ week-6/practice_9/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    let A:i32 = 10;
+    let B:i32 = 20;
+
+    println!("Value of A:{}",A);
+    println!("Value of B:{}",B);
+
+    let mut res = A>B;
+    println!("A greater than B: {}",res);
+
+    res = A<B;
+    println!("A is lesser than B: {}",res);
+
+    res = A>B;
+    println!("A is greater than B: {}",res);
+
+    res = A>=B;
+    println!("A is greater than or equal to B: {}",res);
+
+    res = A<=B;
+    println!("A is lesser than or equal to B: {}",res);
+
+    res = A==B;
+    println!("A is equal B: {}",res);
+
+    res = A!=B;
+    println!("A is not equal B: {}",res);
 }
